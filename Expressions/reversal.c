cd72e729016c22ffcc2a4632cd3cc92210d187ec
Emma <156218556+em-baggie@users.noreply.github.com>
2024-07-23 17:51:02 +0100

    expressions code added

---
+++ Expressions/reversal.c
@@ -0,0 +1,16 @@
+// reverses a 3-digit number
+
+#include <stdio.h>
+
+int main(void) {
+    int num, first, second, third;
+
+    printf("Enter a three-digit number: ");
+    scanf("%d", &num);
+
+    third = num % 10;
+    second = (num / 10) % 10;  
+    first = num / 100;
+
+    printf("The reversal is: %d%d%d\n", third, second, first);
+}
