18f08b2fd088b981695dda86a5d33cbc06ec0392
Emma <156218556+em-baggie@users.noreply.github.com>
2024-08-26 18:02:43 +0100

    repdigit third version

--- Arrays/repdigit.c
+++ Arrays/repdigit.c
@@ -4,30 +4,40 @@
 #include <stdbool.h>
 
 int main(void) {
-    int digit_count[10] = {0};
+    int digit_count[10];
     int digit;
-    long n;
+    long n, ncpy;
 
-    printf("Enter a number: ");
-    scanf("%ld", &n);
+    do {
 
-    printf("Digit:      ");
-    for (int i = 0; i < 10; i++) {
-        printf("%d ", i);
-    }
-    printf("\n");
-    
-    printf("Occurences: ");
-    while (n > 0) {
-        digit = n % 10;
-        digit_count[digit] += 1;
-        n /= 10;
-    }
-    
-    for (int i = 0; i < 10; i++) {
-        printf("%d ", digit_count[i]);
-    }
-    printf("\n");
+        for (int i = 0; i < 10; i++) {
+            digit_count[i] = 0;
+        }
+
+        printf("Enter a number: ");
+        scanf("%ld", &n);
+
+        ncpy = n;
+
+        printf("Digit:      ");
+        for (int i = 0; i < 10; i++) {
+            printf("%d ", i);
+        }
+        printf("\n");
+        
+        printf("Occurences: ");
+        while (ncpy > 0) {
+            digit = ncpy % 10;
+            digit_count[digit] += 1;
+            ncpy /= 10;
+        }
+        
+        for (int i = 0; i < 10; i++) {
+            printf("%d ", digit_count[i]);
+        }
+        printf("\n");
+
+    } while (n > 0);
 
     return 0;
 }
