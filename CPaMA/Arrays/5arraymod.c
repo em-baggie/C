9cc2f53b369b9465a7174c211fd1e36e19328a35
Emma <156218556+em-baggie@users.noreply.github.com>
2024-09-02 20:45:22 +0100

    finished 5arraymod.c

--- CPaMA/Arrays/5arraymod.c
+++ CPaMA/Arrays/5arraymod.c
@@ -5,19 +5,22 @@ and the average score. high score, and low score for each quiz.
 */
 
 #include <stdio.h>
-#define NUM 5
+#define GRID_SIZE 5
+#define MAX_NUM 100
+#define MIN_NUM 0
+
 
 int main(void) {
-    int grid[NUM][NUM] = {0};
-    int totalr[NUM] = {0};
-    int totalc[NUM] = {0};
+    int grid[GRID_SIZE][GRID_SIZE] = {0};
+    int totalr[GRID_SIZE] = {0};
+    int totalc[GRID_SIZE] = {0};
     int totalrr = 0;
-    int total_quiz[NUM] = {0};
-    float avg_quiz[NUM] = {0.0};
-    int hs[NUM] = {-1};
-    int ls[NUM] = {100};
+    int total_quiz[GRID_SIZE] = {0};
+    float avg_quiz[GRID_SIZE] = {0.0};
+    int hs[GRID_SIZE] = {MIN_NUM, MIN_NUM, MIN_NUM, MIN_NUM, MIN_NUM};
+    int ls[GRID_SIZE] = {MAX_NUM, MAX_NUM, MAX_NUM, MAX_NUM, MAX_NUM};
     
-    for (int i = 0; i < NUM; i++) {
+    for (int i = 0; i < GRID_SIZE; i++) {
         printf("Enter score for student %d: ", (i + 1));
         for (int j = 0; j < 5; j++) {
             scanf("%d", &grid[i][j]);
@@ -26,26 +29,26 @@ int main(void) {
             }
         }
 
-    for (int i = 0; i < NUM; i++) {
+    for (int i = 0; i < GRID_SIZE; i++) {
         for (int j = 0; j < 5; j++) {
             total_quiz[i] += grid[i][j];
         }
     }
 
-    for (int i = 0; i < NUM; i++) {
-        avg_quiz[i] = (float) total_quiz[i] / NUM;
+    for (int i = 0; i < GRID_SIZE; i++) {
+        avg_quiz[i] = (float) total_quiz[i] / GRID_SIZE;
     }
 
-    for (int i = 0; i < NUM; i++) {
-        for (int j = 0; j < NUM; j++) {
+    for (int i = 0; i < GRID_SIZE; i++) {
+        for (int j = 0; j < GRID_SIZE; j++) {
             if (grid[i][j] > hs[i]) {
                 hs[i] = grid[i][j];
             }
         }
     }
 
-    for (int i = 0; i < NUM; i++) {
-        for (int j = 0; j < NUM; j++) {
+    for (int i = 0; i < GRID_SIZE; i++) {
+        for (int j = 0; j < GRID_SIZE; j++) {
             if (grid[i][j] < ls[i]) {
                 ls[i] = grid[i][j];
             }
@@ -53,7 +56,7 @@ int main(void) {
     }
 
     printf("Total score for each student: %d %d %d %d %d\n", totalc[0], totalc[1], totalc[2], totalc[3], totalc[4]);
-    printf("Average score for each student: %.2f %.2f %.2f %.2f %.2f\n", (float) totalc[0]/NUM, (float) totalc[1]/NUM, (float) totalc[2]/NUM, (float) totalc[3]/NUM, (float) totalc[4]/NUM);
+    printf("Average score for each student: %.2f %.2f %.2f %.2f %.2f\n", (float) totalc[0]/GRID_SIZE, (float) totalc[1]/GRID_SIZE, (float) totalc[2]/GRID_SIZE, (float) totalc[3]/GRID_SIZE, (float) totalc[4]/GRID_SIZE);
     printf("Average score for each quiz: %.2f %.2f %.2f %.2f %.2f\n", avg_quiz[0], avg_quiz[1], avg_quiz[2], avg_quiz[3], avg_quiz[4]);
     printf("High score for each quiz: %d %d %d %d %d\n", hs[0], hs[1], hs[2], hs[3], hs[4]);
     printf("Low score for each quiz: %d %d %d %d %d\n", ls[0], ls[1], ls[2], ls[3], ls[4]);
