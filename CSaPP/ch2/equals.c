3d5235a7e4a7438fc8815eadde967c6184263a5a
Emma <156218556+em-baggie@users.noreply.github.com>
2024-09-26 10:51:05 +0100

    more CSaPP exercises

--- CSaPP/ch2/equals.c
+++ CSaPP/ch2/equals.c
@@ -1,6 +1,7 @@
 // Using only bit-level and logical operations, write a C expression that is equivalent to x == y. In other words, it will return 1 when x and y are equal and 0 otherwise.
 
 #include <stdio.h>
+#include <stdint.h>
 
 int main(void) {
     int x, y, equals;
